45d9734993e2b5d81e2e2e4498845757ce7f234d
Lachie Dwyer <lachiedwyer123@gmail.com>
2024-11-05 17:53:53 +1000

    Went back a few features but made GUI

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import tkinter as tk
 import random
 
 loot = [('1 Star', 500), ('2 Star', 350), ('3 Star', 144), ('Limit Broken', 6)]
-three_star_pity_loot = [('3 Star', 1)]
+three_star_pity_loot = [('3 Star', 100)]
 limit_broken_pity_loot = [('Limit Broken', 1)]
 vowels = ('a', 'e', 'i', 'o', 'u')
 pull_total = 0
@@ -22,53 +23,76 @@ for item, weight in limit_broken_pity_loot:
     limit_broken_pity_choices.extend([item] * weight)
 
 def pull():
-    global three_star_pity_counter, limit_broken_pity_counter
+    global three_star_pity_counter, limit_broken_pity_counter, pull_total
     if limit_broken_pity_counter < 100:
-        if three_star_pity_counter < 10:
-            pull = random.choice(choices)
-            if pull == '3 Star':
-                three_star_pity_counter = 0  
+        if three_star_pity_counter < 9:
+            result = random.choice(choices)
+            if result == '3 Star':
+                three_star_pity_counter = 0
             else:
-                three_star_pity_counter += 1  
+                three_star_pity_counter += 1
         else:
-            pull = random.choice(three_star_pity_choices)
-            three_star_pity_counter = 0  
-
-        if pull == 'Limit Broken':
-            limit_broken_pity_counter = 0  
-        else:
-            limit_broken_pity_counter += 1  
+            result = random.choice(three_star_pity_choices)
+            three_star_pity_counter = 0
     else:
-        pull = random.choice(limit_broken_pity_choices)
-        limit_broken_pity_counter = 0  
+        result = random.choice(limit_broken_pity_choices)
+        limit_broken_pity_counter = 0
 
-    if pull.lower().startswith(vowels):
-        print("You got an " + str(pull))
+    if result == 'Limit Broken':
+        limit_broken_pity_counter = 0
     else:
-        print("You got a " + str(pull))
-    obtained_loot.append(pull)
+        limit_broken_pity_counter += 1
 
-while True:
-    player_choice = input("What do you want to do (Help for commands): ")
+    pull_total += 1
+    obtained_loot.append(result)
+
+    if result.lower().startswith(vowels):
+        result_text = f"You got an {result}"
+    else:
+        result_text = f"You got a {result}"
+
+    pull_result_label.config(text=result_text)
+    pull_total_label.config(text=f"Total Pulls: {pull_total}")
+
+    update_loot_count()
     
-    if player_choice.lower() == "pull":
+def pullTen():
+    for i in range(10):
         pull()
-        pull_total += 1
-    elif player_choice.lower() == "help":
-        print(" Pull - Pull 1 Card \n Pull 10 - Pull 10 Cards \n Pull Total - Check your total amounts of pulls \n Obtained Loot - Check what loot you have obtained \n Show Loot Table - View the odds")
-    elif player_choice.lower() == "show loot table":
-        print(" A = 20% \n B = 20% \n C = 50% \n D = 10% \n E = 1%")
-    elif player_choice.lower() == "pull 10" or player_choice.lower() == "10 pull":
-        for i in range(10):
-            pull()
-            pull_total += 1
-    elif player_choice.lower() == "pull total" or player_choice.lower() == "total pull":
-        print("You have pulled a total of " + str(pull_total) + " times")
-    elif player_choice.lower() == "obtained loot":
-        print("You have " + str(obtained_loot.count('1 Star')) + " 1 Stars")
-        print("You have " + str(obtained_loot.count('2 Star')) + " 2 Stars")
-        print("You have " + str(obtained_loot.count('3 Star')) + " 3 Stars")
-        print("You have " + str(obtained_loot.count('Limit Broken')) + " Limit Broken")
-    elif player_choice.lower() == "pity":
-        print("Three Star Pity: " + str(three_star_pity_counter))
-        print("Limit Broken Pity: " + str(limit_broken_pity_counter))
+
+def update_loot_count():
+    loot_count_text = (
+        f"1 Star: {obtained_loot.count('1 Star')}\n"
+        f"2 Star: {obtained_loot.count('2 Star')}\n"
+        f"3 Star: {obtained_loot.count('3 Star')}\n"
+        f"Limit Broken: {obtained_loot.count('Limit Broken')}"
+    )
+    loot_count_label.config(text=loot_count_text)
+
+# Set up the main window
+root = tk.Tk()
+root.title("Pull Simulator")
+root.geometry("300x300")
+
+# Display pull result
+pull_result_label = tk.Label(root, text="Just press the button")
+pull_result_label.pack(pady=10)
+
+# Display total pulls
+pull_total_label = tk.Label(root, text="Total Pulls: 0")
+pull_total_label.pack(pady=10)
+
+# Pull button
+pull_button = tk.Button(root, text="Pull", command=pull)
+pull_button.pack(pady=10)
+
+# Pull 10 button
+pull_ten_button = tk.Button(root, text="Pull x10", command=pullTen)
+pull_ten_button.pack(pady=10)
+
+# Display obtained loot counts
+loot_count_label = tk.Label(root, text="1 Star: 0\n2 Star: 0\n3 Star: 0\nLimit Broken: 0")
+loot_count_label.pack(pady=10)
+
+# Run the main event loop
+root.mainloop()
